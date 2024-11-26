e56e9424d5917f1c05286d56415a0bd540f96098
zesongguo <zguo283@wisc.edu>
2024-11-26 02:32:34 -0800

    Add auth controllers

--- backend/controllers/auth.contoller.js
+++ backend/controllers/auth.contoller.js
@@ -1,17 +1,111 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.json({
-        data: "signup endpoint",
-    });
-}
+    try {
+        const { fullName, username, email, password } = req.body;
+
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            console.log(`${email}`);
+            return res.status(400).json({error: "Invalid email format!"});
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({error: "This username is already taken!"});
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({error: "This email is already taken!"});
+        }
+
+        if (password < 6) {
+            return res.status(400).json({ error: "Password should be at least 6 characters long!"});
+        }
+
+        // Need to hash password to make it unreadable
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            username,
+            email,
+            password: hashPassword
+        });
+
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save(); // save the user to database
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg,
+            });
+
+        } else {
+            res.status(400).json({ error: "Invalid user data!" });
+        }
+
+    } catch (error) {
+        console.log("Error in signup auth.controller", error.message);
+        res.status(500).json({ error: "Internal server error"});
+    }
+};
 
 export const login = async (req, res) => {
-    res.json({
-        data: "login endpoint",
-    });
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        const validPassword = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !validPassword) {
+            return res.status(400).json({ error: "Invalid password or username!"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			email: user.email,
+			followers: user.followers,
+			following: user.following,
+			profileImg: user.profileImg,
+			coverImg: user.coverImg,
+		});
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
 
 export const logout = async (req, res) => {
-    res.json({
-        data: "logout endpoint",
-    });
+    try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
+
+export const getMe = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id).select("-password"); // no password return
+		res.status(200).json(user);
+	} catch (error) {
+		console.log("Error in getMe controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,22 @@ import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middleware to parse req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // parse form data
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 })
